25338fb3f6703cc21e0b2757c58b5901657cefe9
Ariel Liriano <ariellirianot989@gmail.com>
2022-06-07 17:26:22 -0400

    Validación de formularios incluida

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilos.css">
-    <title>Insertar Con ajx</title>
+    <title>Insertar Con ajax</title>
 </head>
 
 <body>
@@ -14,16 +14,16 @@
     <div class="contenedor">
         <form id="frmAjax" method="post">
             <label for="nombre">Nombre</label>
-            <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+            <input type="text" name="nombre" id="nombre" placeholder="Nombre" required>
 
             <label for="Apellido">Apellido</label>
-            <input type="text" name="apellido" id="apellido" placeholder="Apellido">
+            <input type="text" name="apellido" id="apellido" placeholder="Apellido" required>
 
             <label for="usuario">Usuario</label>
-            <input type="text" name="usuario" id="usuario" placeholder="Usuario">
+            <input type="text" name="usuario" id="usuario" placeholder="Usuario" required>
 
             <label for="password">Contraseña</label>
-            <input type="password" name="password" id="password" placeholder="Contraseña">
+            <input type="password" name="password" id="password" placeholder="Contraseña" required>
 
             <button id="btnGuardar">Guardar Datos</button>
 
--- js/app.js
+++ js/app.js
@@ -2,21 +2,38 @@ $(document).ready(function () {
   var form = document.getElementById("frmAjax");
   $("#btnGuardar").click(function () {
     var datos = $("#frmAjax").serialize();
+    var validarF = validarFormulario();
 
-    $.ajax({
-      type: "POST",
-      url: "insertar.php",
-      data: datos,
-      success: function (respuesta) {
-        if (respuesta == 1) {
-          alert("Datos agregados exitosamente!");
-          form.reset();
-        } else {
-          alert("Error al registrar los Datos");
-        }
-      },
-    });
+    if (validarF == 1) {
+      $.ajax({
+        type: "POST",
+        url: "insertar.php",
+        data: datos,
+        success: function (respuesta) {
+          if (respuesta == 1) {
+            alert("Datos agregados exitosamente!");
+            form.reset();
+          } else {
+            alert("Error al registrar los Datos");
+          }
+        },
+      });
+    } else {
+      alert("No deben de haber campos vaciós para guardar la información");
+    }
 
     return false;
   });
+
+  function validarFormulario() {
+    var nombre = $("#nombre").val();
+    var apellido = $("#apellido").val();
+    var usuario = $("#usuario").val();
+    var password = $("#password").val();
+    if (nombre != "" && apellido != "" && usuario != "" && password != "") {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 });
